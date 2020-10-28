40440796bc154432183e6379d77f29713767588f
John Karima <karimajohn24@gmail.com>
2020-10-28 21:29:42 +0300

    link angular app to django backend

---
+++ Tutorial/src/app/components/add-tutorial/add-tutorial.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { TutorialService } from 'src/app/services/tutorial.service';
+
+
+@Component({
+    selector: 'app-add-tutorial',
+    templateUrl: './add-tutorial.component.html',
+    styleUrls: ['./add-tutorial.component.css']
+  })
+  export class AddTutorialComponent implements OnInit {
+    tutorial = {
+      title: '',
+      description: '',
+      tutorial_image: '',
+      tutorial_content: '',
+      tutorial_author: '',
+      published: false
+    };
+    submitted = false;
+  
+    constructor(private tutorialService: TutorialService) { }
+  
+    ngOnInit() {
+    }
+  
+    saveTutorial() {
+      const data = {
+        title: this.tutorial.title,
+        description: this.tutorial.description,
+        tutorial_image: this.tutorial.tutorial_image,
+        tutorial_content: this.tutorial.tutorial_content,
+        tutorial_author: this.tutorial.tutorial_author,
+      };
+  
+      this.tutorialService.create(data)
+        .subscribe(
+          response => {
+            console.log(response);
+            this.submitted = true;
+          },
+          error => {
+            console.log(error);
+          });
+    }
+  
+    newTutorial() {
+      this.submitted = false;
+      this.tutorial = {
+        title: '',
+        description: '',
+        tutorial_image: '',
+        tutorial_content: '',
+        tutorial_author: '',
+        published: false
+    
+      };
+    }
+  }
